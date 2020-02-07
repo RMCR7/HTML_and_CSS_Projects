aff8db60051e5c7bbbb5063933b18e0b26f3d783
Jose Vasquez <jose.vasquez4011@gmail.com>
2020-02-06 22:30:47 -0800

    Delete Basic_JavaScript_1.js

--- Basic_JavaScript_1.js
+++
@@ -1,9 +0,0 @@
-alert("Hello, World! I am an alert box!");
-<script>
-    function My_First_Function() {
-      var string ="Kiss me, I'm Irish!";
-      var result = String.fontcolor("green");
-      document.getElementbyId("Irish") .innerHTML =
-      result;
-    }
-</script>
