3c87634b8a55a9281bcfc2f771fbbb49c7938795
Jose Vasquez <jose.vasquez4011@gmail.com>
2020-02-06 22:29:04 -0800

    Course Assignment
    Please advice if I have done this correctly, 
    thank you.

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Hello, World! I am an alert box!");
+<script>
+    function My_First_Function() {
+      var string ="Kiss me, I'm Irish!";
+      var result = String.fontcolor("green");
+      document.getElementbyId("Irish") .innerHTML =
+      result;
+    }
+</script>
